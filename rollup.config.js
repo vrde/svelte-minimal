f098490398e086547a87a0d4b46cf6decaf214ae
vrde <agranzot@gmail.com>
2020-05-07 15:22:35 +0200

    Update deps

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import json from "@rollup/plugin-json";
 import replace from "@rollup/plugin-replace";
 import svelte from "rollup-plugin-svelte";
 import copy from "rollup-plugin-copy";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
